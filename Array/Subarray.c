bd1bf63ea138b1e54272fb97ccca95840b23aa68
Pranav Shinde <pranavshinde.as@gmail.com>
2024-11-26 17:21:44 +0530

    kadane's algorithm the maximum sub array sum

--- Array/Subarray.c
+++ Array/Subarray.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
+
+int max(int a, int b){
+    if (a > b) return a;
+    else return b;
+}
 // maximum subarray - time complexity O(n^3)
 void subArrays(int array[], int size)
 {
@@ -19,15 +25,30 @@ void subArrays(int array[], int size)
 
 // maximum subarray sum
 // brute force approach - time complexity O(n^2)
-int maxSubArraySum(int array[], int size){
-    int maxSum = __WINT_MIN__;
+void maxSubArraySum(int array[], int size){
+    int maxSum = -(__INT_MAX__);
      for (int i = 0; i < size; i++){
+        int currentSum = 0;
         for (int j = i; j < size; j++){
-            
-          
+            currentSum += array[j];
+            maxSum = max(currentSum, maxSum);
         }
        
     }
+    printf("Max Subarray Sum : %d\n", maxSum);
+}
+
+
+// kadanes algorithm
+void kadaneAlgo(int array[], int size){
+    
+    int currentSum = 0, maxSum = -(__INT_MAX__);
+    for(int i = 0; i < size; i++){
+        currentSum += array[i];
+        maxSum = max(currentSum, maxSum);
+        if(currentSum <0) currentSum = 0;
+    }
+    printf("%d", maxSum);
 }
 int main()
 {
@@ -36,5 +57,6 @@ int main()
 
     int anotherArray[7] = {3, -4, 5, 4, -1, 7, -8};
     maxSubArraySum(anotherArray, 7);
+    kadaneAlgo(anotherArray, 7);
     return 0;
 }
