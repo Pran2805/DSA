c554513e88c6d596c26b44f8f570f962e4891746
Pran2805 <pranavshinde.as@gmail.com>
2026-02-12 11:31:56 +0530

    first commit

---
+++ src/logicBuilding/CheckEvenOrOdd.java
@@ -0,0 +1,22 @@
+package logicBuilding;
+
+import java.util.Scanner;
+
+public class CheckEvenOrOdd {
+
+    public static boolean checkNumber(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to check it is a even or odd");
+        int num = sc.nextInt();
+        System.out.println(checkNumber(num));
+    }
+
+}
